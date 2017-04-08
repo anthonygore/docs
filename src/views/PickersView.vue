ddb3fa4182833ec00b4dde811c7162d516b775ba
John Leider <john.j.leider@gmail.com>
2017-04-08 14:33:04 -0400

    init commit for pickers

---
+++ src/views/PickersView.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  doc-view(v-bind:doc="doc" id="pickers-view")
+    v-date-time
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        doc: {
+          title: 'Pickers',
+          desc: '',
+          props: {}
+        }
+      }
+    },
+
+    mounted () {
+      this.$emit('view', this.meta())
+    },
+
+    preFetch () {
+      return this.methods.meta()
+    },
+
+    methods: {
+      meta () {
+        return {
+          title: 'Pickers | Vuetify.js',
+          h1: 'Pickers',
+          description: 'Pickers directive for Vuetify Framework',
+          keywords: 'vuetify, pickers, directives'
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+</style>
