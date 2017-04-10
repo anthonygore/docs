900b90e96c06a7301437d274f8695e21f45f8b13
John Leider <john.j.leider@gmail.com>
2017-04-10 18:28:40 -0400

    doc update

--- src/views/PickersView.vue
+++ src/views/PickersView.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
   doc-view(v-bind:doc="doc" id="pickers-view")
-    v-menu(transition="v-scale-transition" v-bind:close-on-content-click="false" v-model="menu")
+    v-menu(transition="v-scale-transition" v-bind:close-on-content-click="false" lazy)
       v-text-field(slot="activator" label="Date" single-line v-model="test")
-      v-date-picker(v-model="test")
+      v-date-picker(v-model="test" dark)
         template(scope="{ save, cancel }")
-          v-card-row(actions v-on:click.native="menu = false")
+          v-card-row(actions)
             v-btn(flat primary v-on:click.native="cancel()") Cancel
             v-btn(flat primary v-on:click.native="save()") Ok
-    v-modal(v-model="modal")
+    v-modal(lazy)
       v-text-field(slot="activator" label="Date" single-line v-model="test")
       v-date-picker(v-model="test")
         template(scope="{ save, cancel }")
-          v-card-row(actions v-on:click.native="modal = false")
+          v-card-row(actions)
             v-btn(flat primary v-on:click.native="cancel()") Cancel
             v-btn(flat primary v-on:click.native="save()") Ok
 </template>
