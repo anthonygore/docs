31b84e217cfc1efe191b03199822709e78e451be
John Leider <john.j.leider@gmail.com>
2017-04-10 15:28:38 -0400

    updated docs

--- src/views/PickersView.vue
+++ src/views/PickersView.vue
@@ -1,12 +1,28 @@
 <template lang="pug">
   doc-view(v-bind:doc="doc" id="pickers-view")
-    v-date-picker
+    v-menu(transition="v-scale-transition" v-bind:close-on-content-click="false" v-model="menu")
+      v-text-field(slot="activator" label="Date" single-line v-model="test")
+      v-date-picker(v-model="test")
+        template(scope="{ save, cancel }")
+          v-card-row(actions v-on:click.native="menu = false")
+            v-btn(flat primary v-on:click.native="cancel()") Cancel
+            v-btn(flat primary v-on:click.native="save()") Ok
+    v-modal(v-model="modal")
+      v-text-field(slot="activator" label="Date" single-line v-model="test")
+      v-date-picker(v-model="test")
+        template(scope="{ save, cancel }")
+          v-card-row(actions v-on:click.native="modal = false")
+            v-btn(flat primary v-on:click.native="cancel()") Cancel
+            v-btn(flat primary v-on:click.native="save()") Ok
 </template>
 
 <script>
   export default {
     data () {
       return {
+        test: '2007-09-01',
+        menu: false,
+        modal: false,
         doc: {
           title: 'Pickers',
           desc: '',
@@ -24,6 +40,9 @@
     },
 
     methods: {
+      testing (e) {
+        e.cancel()
+      },
       meta () {
         return {
           title: 'Pickers | Vuetify.js',
@@ -37,4 +56,6 @@
 </script>
 
 <style lang="stylus">
+  #pickers-view
+    min-height: 150vh
 </style>
